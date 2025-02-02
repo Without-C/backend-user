d8a2c5e1f73d55dec3861f50a659a0fad2146720
Youngho Cho <dominoyh5@gmail.com>
2025-02-02 15:06:11 +0900

    feat: add 42 oauth callback view

--- auth/urls.py
+++ auth/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('test/', views.test_view, name='test_view'),
     path('login/42/', views.login_redirect_42, name='login_redirect_42'),
+    path('callback/42/', views.callback_42, name='callback_42'),
 ]
--- auth/views.py
+++ auth/views.py
@@ -1,4 +1,5 @@
 import json
+import requests
 from django.shortcuts import redirect
 from django.conf import settings
 from django.http import JsonResponse
@@ -28,3 +29,27 @@ def test_view(request):
 def login_redirect_42(request):
     redirect_uri = f'https://api.intra.42.fr/oauth/authorize?client_id={settings.OAUTH_UID_42}&redirect_uri={settings.OAUTH_REDIRECT_42}&response_type=code&response_type=code'
     return redirect(redirect_uri)
+
+def callback_42(request):
+    code = request.GET.get('code')
+
+    # Obtain user's access code from 42 api
+    response = requests.post("https://api.intra.42.fr/oauth/token", data={
+        'grant_type': 'authorization_code',
+        'client_id': settings.OAUTH_UID_42,
+        'client_secret': settings.OAUTH_SECRET_42,
+        'code': code,
+        'redirect_uri': settings.OAUTH_REDIRECT_42,
+    })
+
+    if response.status_code != 200:
+        # 42 서버로부터 access code 받아오는 것을 실패하면 어떻게 예외 처리하지?
+        return redirect('/')
+
+    access_token = response.json()['access_token']
+
+    # profile = get_profile_42(access_token)
+    # print(profile.login)
+    # print(profile.image)
+
+    return redirect('/')
