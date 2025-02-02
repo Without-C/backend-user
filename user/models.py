4a1e62df01811400e4009e8f3b0b5e995cb41a19
Youngho Cho <dominoyh5@gmail.com>
2025-02-02 17:31:15 +0900

    feat: add oauth_id_42 field to CustomUser

--- user/models.py
+++ user/models.py
@@ -2,4 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
+    oauth_id_42 = models.CharField(
+        max_length=255,
+        null=True,
+    )
     avatar = models.FileField(null=True, upload_to='avatars/')
