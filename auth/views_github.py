a65c5aa458235233244265d8277402f76c6bd8b7
Youngho Cho <dominoyh5@gmail.com>
2025-02-04 23:32:07 +0900

    refactor: remove duplicated functions

--- auth/views_github.py
+++ auth/views_github.py
@@ -1,12 +1,11 @@
 import os
-import uuid
 import requests
-from urllib.parse import urlparse
 from django.shortcuts import redirect
 from django.conf import settings
 from django.core.files.base import ContentFile
 from user.models import CustomUser
 from rest_framework_simplejwt.tokens import RefreshToken
+from .utils import get_image_file, get_random_filename
 
 def login_redirect_github(request):
     """
@@ -64,24 +63,6 @@ def callback_github(request):
     return response
 
 def get_profile_github(access_token):
-    def get_image_file(image_url):
-        """
-        주어진 image_url으로부터 사진 다운로드
-        """
-        response = requests.get(image_url)
-        if response.status_code != 200:
-            return None
-        return response.content
-
-    def get_random_filename(image_url):
-        """
-        주어진 image_url으로부터 얻은 이미지 확장자를 유지하며, 무작위 파일 이름 생성
-        """
-        original_filename = os.path.basename(urlparse(image_url).path)
-        _, ext = os.path.splitext(original_filename)
-        random_filename = uuid.uuid4().hex
-        return f"{random_filename}{ext}"
-
     # access token을 이용하여 42 서버에 사용자 정보 요청
     response = requests.get('https://api.github.com/user', headers={'Authorization': f'Bearer {access_token}'})
     if response.status_code != 200:
